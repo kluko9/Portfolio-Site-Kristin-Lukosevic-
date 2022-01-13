ddaa368bf829f7c095d5837733dc46ba11040952
kluko9 <klukosevic@gmail.com>
2022-01-13 17:29:47 -0600

    added js to portfolio site

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 
 console.log("Hi there welcome to my Portfolio Site");
 
-alert("Hi there welcome to my Portfolio site");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+   x.className += ' responsive';
+ } else {
+   x.className = 'navtoggle';
+ }
+}
