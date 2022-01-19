a18be307843196f8bdf5fbe542e0c8a447635841
kluko9 <klukosevic@gmail.com>
2022-01-19 17:50:44 -0600

    website preparation

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 
-console.log("Hi there welcome to my Portfolio Site");
+console.log('Hi there welcome to my Portfolio Site')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-   x.className += ' responsive';
- } else {
-   x.className = 'navtoggle';
- }
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
 }
